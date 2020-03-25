2cd5fcea80adfc65a0aeed7dbd346083c5ad74bd
Nikita Lebedev <y87cgp@gmail.com>
2020-03-25 20:50:52 +0300

    Дописал README, добавил папку docs

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,5 +1,6 @@
 CREATE TABLE employees (
-    emp_id      SERIAL  PRIMARY KEY,
+    emp_id      SERIAL  ,
+    -- PRIMARY KEY,
     surname     TEXT    NOT NULL,
     name        TEXT    NOT NULL,
     patronymic  TEXT,
@@ -7,24 +8,31 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE divisions (
-    div_id      SERIAL PRIMARY KEY,
+    div_id      SERIAL ,
+    -- PRIMARY KEY,
     div_name    TEXT,
-    head_div_id INT REFERENCES divisions(div_id) ON DELETE CASCADE,
-    chief_id    INT REFERENCES employees(emp_id)
+    head_div_id INT ,
+    -- REFERENCES divisions(div_id) ON DELETE CASCADE,
+    chief_id    INT
+    -- REFERENCES employees(emp_id)
 );
 
 CREATE TABLE positions (
-    pos_id           SERIAL PRIMARY KEY,
-    div_id           INT    REFERENCES divisions(div_id),
+    pos_id           SERIAL ,
+    -- PRIMARY KEY,
+    div_id           INT    ,
+    -- REFERENCES divisions(div_id),
     pos_name         TEXT,
     responsibilities TEXT
 );
 
 CREATE TABLE emp_pos (
-    emp_id  INT REFERENCES employees(emp_id),
-    pos_id  INT REFERENCES positions(pos_id),
+    emp_pos_id SERIAL,
+    emp_id  INT ,
+    -- REFERENCES employees(emp_id),
+    pos_id  INT ,
+    -- REFERENCES positions(pos_id),
     start_date  DATE,
     finish_date DATE,
     salary  INT
 );
-
--- db/fill_tables.sql
+++
@@ -1,4 +0,0 @@
-\COPY employees FROM 'db/out_emp.csv' WITH (FORMAT csv, DELIMITER ',', NULL 'null');
-\COPY divisions FROM 'db/out_div.csv' WITH (FORMAT csv, DELIMITER ',', NULL 'null');
-\COPY positions FROM 'db/out_pos.csv' WITH (FORMAT csv, DELIMITER ',', NULL 'null');
-\COPY emp_pos FROM 'db/out_emp_pos.csv' WITH (FORMAT csv, DELIMITER ',', NULL 'null');
